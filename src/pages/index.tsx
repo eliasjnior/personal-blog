05ee7bf1d01a2b252333de2b29faeb199f9ed9e2
Elias Júnior <me@eliasjr.dev>
2020-07-03 19:41:08 -0300

    feat: add style to post list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,19 @@ import { GrGithub, GrLinkedinOption } from 'react-icons/gr'
 import GlobalStyle from '~/styles/GlobalStyle'
 
 import {
+  Content,
   Header,
   Image,
   Menu,
   MenuLink,
+  Post,
+  PostContent,
+  PostDate,
+  PostDescription,
+  PostImage,
+  PostLink,
+  PostList,
+  PostTitle,
   Presentation,
   Separator,
   SocialLink,
@@ -45,6 +54,28 @@ const Home: React.FC = () => {
           <MenuLink href="#">Contact</MenuLink>
         </Menu>
       </Header>
+      <Content>
+        <PostList>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((post) => (
+            <Post key={post.toString()}>
+              <PostImage src="https://via.placeholder.com/150x150" />
+              <PostContent>
+                <PostTitle>
+                  <a href="#">Why Every Developer Should Have A Blog</a>
+                </PostTitle>
+                <PostDate>Published 2 days ago</PostDate>
+                <PostDescription>
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                  natoque penatibus et magnis dis parturient montes, nascetur
+                  ridiculus mus. Donec quam felis, ultricies...
+                </PostDescription>
+                <PostLink href="#">Read more →</PostLink>
+              </PostContent>
+            </Post>
+          ))}
+        </PostList>
+      </Content>
     </>
   )
 }
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,3 +1,4 @@
+import { darken } from 'polished'
 import styled, { css } from 'styled-components'
 
 export const Header = styled.header`
@@ -6,6 +7,9 @@ export const Header = styled.header`
   max-width: 300px;
   min-height: 100vh;
   padding: 30px;
+  position: fixed;
+  top: 0;
+  left: 0;
 `
 
 export const Title = styled.h1`
@@ -95,4 +99,81 @@ export const MenuLink = styled.a<MenuLinkProps>`
             background-color: rgba(255, 255, 255, 0.05);
           }
         `}
+`
+
+export const Content = styled.div`
+  margin-left: 300px;
+  padding: 30px;
+`
+
+export const PostList = styled.div``
+
+export const Post = styled.div`
+  display: flex;
+  &:not(:last-child) {
+    margin-bottom: 30px;
+  }
+`
+
+export const PostImage = styled.img`
+  margin-right: 15px;
+  user-drag: none;
+  user-select: none;
+`
+
+export const PostContent = styled.div``
+
+export const PostTitle = styled.h1`
+  font-size: 1.5em;
+  font-weight: 500;
+  margin-bottom: 10px;
+  color: #333;
+
+  a {
+    display: inline-block;
+    position: relative;
+
+    &:after {
+      content: '';
+      width: 0;
+      height: 2px;
+      background-color: #333;
+      position: absolute;
+      left: 0;
+      bottom: -5px;
+      transition: all ease-in-out 0.2s;
+      opacity: 0;
+    }
+
+    &:hover,
+    &:focus {
+      &:after {
+        width: 100%;
+        opacity: 1;
+      }
+    }
+  }
+`
+
+export const PostDate = styled.p`
+  font-size: 0.8rem;
+  color: #8f8f8f;
+  margin-bottom: 10px;
+  font-weight: 300;
+`
+
+export const PostDescription = styled.p`
+  color: #4f4f4f;
+  font-weight: 300;
+  margin-bottom: 10px;
+`
+
+export const PostLink = styled.a`
+  color: #5f8fcb;
+  transition: all linear 0.2s;
+
+  &:hover,
+  &:focus {
+    color: ${darken(0.2, '#5f8fcb')};
+  }
 `
